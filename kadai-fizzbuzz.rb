17598166f39ce96d0936067f1f8b11824ff77019
Kame Asari <asaneko22@gmail.com>
2022-01-27 20:25:57 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,36 @@
+num_max = 100
+
+def fizzbuzz(num)
+  if num % 15 == 0
+    return "FizzBuzz"
+  elsif num % 5 == 0
+    return "Buzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  else
+    return num.to_s
+  end
+end
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
+
+
+num_max = 100
+
+def fizzbuzz(num)
+  if num % 15 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else 
+    num.to_s
+  end
+end
+
+(1..100).each do |num|
+  puts fizzbuzz(num)
+end
